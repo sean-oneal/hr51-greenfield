56246a0f0527cdc08940d92222bee3a54ad4ca9c
sean-oneal <smoneal@gmail.com>
2016-12-08 12:11:43 -0800

    Server Code Template Commented Out

--- server/server.js
+++ server/server.js
@@ -10,17 +10,17 @@ const User = require('./db/models/user');
 User.create({name: 'something', age: 100});
 
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('we\'re connected');
-});
-app.get('/users', function (req, res) {
-  res.send('Hello World!')
-});
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('we\'re connected');
+// });
+// app.get('/users', function (req, res) {
+//   res.send('Hello World!')
+// });
 
-app.get('*', function (req, res) {
-  res.send('Server is listening!')
-})
+// app.get('*', function (req, res) {
+//   res.send('Server is listening!')
+// })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
