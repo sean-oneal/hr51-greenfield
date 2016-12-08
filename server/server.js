46f585bed116acdecca04f669fc69f146f9bbb58
Sean O'Neal <seanoneal@Seans-MacBook-Air.local>
2016-12-07 23:35:07 -0800

    Remove redundent File, Add User Database Schema, Initialize Mongoose Database

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/broccoli');
+var db = mongoose.connection;
+
+const User = require('./db/models/user');
+
+User.create({name: 'something', age: 100});
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we\'re connected');
+});
+app.get('/users', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.get('*', function (req, res) {
+  res.send('Server is listening!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
