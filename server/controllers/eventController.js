a604420182798b48860197dcd900c011301f1123
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-11 15:48:49 -0800

    Create event handler(s) for Eventful api

---
+++ server/controllers/eventController.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const apiKey = require('../apiKey.js');
+
+module.exports = {
+  getEvent : (req, res) => {
+    let category = req.query.category;
+    let date = req.query.date || 'Today';
+    let page = req.query.page || 1;
+    let location = req.query.location || 'USA';
+
+    if (!category) {
+      return res.status(400).send('category must be defined');
+    }
+
+    axios.get('http://api.eventful.com/json/events/search?app_key=' + apiKey + '&c=' + category + '&date=' + date + '&page_size=4' + '&page_number=' + page + '&location=' + location)
+    .then((response) => {
+      console.log('RESPONSE', response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+};
---
+++ server/router.js
@@ -0,0 +1,11 @@
+const eventController = require('./controllers/eventController');
+
+module.exports = (app) => {
+
+  app.get('/api/event', eventController.getEvent);
+
+  app.post('/api/event', (req, res) =>  {
+    res.send('World');
+  });
+
+}
