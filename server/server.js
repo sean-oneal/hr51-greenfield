39b1f06b9d377278c6235ee2e93f2da941da4fe7
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-11 15:48:49 -0800

    Create event handler(s) for Eventful api

--- server/server.js
+++ server/server.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const router = require('./router.js');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/broccoli');
-var db = mongoose.connection;
-
+const db = mongoose.connection;
 const User = require('./db/models/user');
 
-User.create({name: 'something', age: 100});
+// const fakeDATA = {
+// 	name: 'Jennifer',
+// 	searches: String,
+//	savedEvents: Mixed
+// }
+
+//mw
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//will pserve all static files
+app.use(express.static(path.join(__dirname, '../client')));
 
 
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log('we\'re connected');
-// });
-// app.get('/users', function (req, res) {
-//   res.send('Hello World!')
-// });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Connected to database'));
 
-// app.get('*', function (req, res) {
-//   res.send('Server is listening!')
-// })
+router(app);
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 });
