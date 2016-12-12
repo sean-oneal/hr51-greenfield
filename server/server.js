6cbfaaafb2015442201268873d4c7451f05304e3
Jennifer Lê <jnnnle.sf@gmail.com>
2016-12-11 21:04:31 -0800

    Change path of static files

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static('../client'));
 
 router(app);
 
